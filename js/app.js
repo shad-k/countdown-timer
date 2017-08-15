e9fb04794079e560a4d4db37c95ab367e53772d8
shad-k <shadab.2005@gmail.com>
2017-08-15 22:39:50 +0530

    Add kill switch working logic

--- js/app.js
+++ js/app.js
@@ -1,12 +1,49 @@
 $(function() {
-	var endDate = new Date("August 17, 2017 23:59:59");
-	var now = Date.now();
+	var status = localStorage.getItem("status");
+	console.log(status);
 
-	console.log(endDate - now);
-	var difference = endDate - now;
-	difference /= 1000;
-	var clock = $('.your-clock').FlipClock(difference, {
-		clockFace: "DailyCounter",
-		countdown: true
-	});
+	if(status === null) {
+		status = "true";
+		localStorage.setItem("status", "true");
+	}
+
+	if(status === "true") {
+		var endDate = new Date("August 17, 2017 23:59:59");
+		var now = Date.now();
+
+		var difference = endDate - now;
+		difference /= 1000;
+		var clock = $('.your-clock').FlipClock(difference, {
+			clockFace: "DailyCounter",
+			countdown: true
+		});
+
+		$(".killSwitch").click(function() {
+			$(".modal").modal();
+		});
+
+		$(".checkPassword").click(function() {
+			var password = $(".password").val();
+
+			if(isPasswordCorrect(password)) {
+				var html = "<h1> Yayy, friendship is saved!!</h1><iframe src='https://giphy.com/embed/l2Sqb0owUC5s5tz5m' frameBorder='0' class='giphy-embed gif' allowFullScreen></iframe>";
+				$(".content").html(html);
+				$(".modal").modal('toggle');
+				localStorage.setItem("status", "false");
+			} else {
+				alert("You don't know the password! Get out of here!!")
+				$(".modal").modal('toggle');
+			}
+		});
+	} else if(status === "false") {
+		var html = "<h1> Yayy, friendship is saved!!</h1><iframe src='https://giphy.com/embed/l2Sqb0owUC5s5tz5m' frameBorder='0' class='giphy-embed gif' allowFullScreen></iframe>";
+		$(".content").html(html);
+	}
+
+	function isPasswordCorrect(password) {
+		if(password === 'IamAwesome')
+			return true;
+		else
+			return false;
+	}
 });
