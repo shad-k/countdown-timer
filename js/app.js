a3e1c062bee21c43393875b289aef3fb83c9fb46
shad-k <shadab.2005@gmail.com>
2017-08-15 21:14:49 +0530

    Add clock

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
 $(function() {
-	var clock = $('.your-clock').FlipClock({
-		console.log("hi");
+	var endDate = new Date("August 17, 2017 23:59:59");
+	var now = Date.now();
+
+	console.log(endDate - now);
+	var difference = endDate - now;
+	difference /= 1000;
+	var clock = $('.your-clock').FlipClock(difference, {
+		clockFace: "DailyCounter",
+		countdown: true
 	});
 });
